b5d00cc28d3c7492d468265b920fface1b5aab5b
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-05 16:01:59 +0900

    環境 - sassを導入し、SCSSとCSS Moduleを使えるようにする

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.css';
+import style from './App.module.scss';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <section className={`${style.root}`}>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className={`${style.logo}`} alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src={reactLogo}
+            className={`${style.logo} ${style.react}`}
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className={`${style.cart}`}>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -25,10 +29,10 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className={`${style.readTheDocs}`}>
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </section>
   );
 }
 
