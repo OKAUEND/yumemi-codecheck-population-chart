a2aeb5d4fe0126a409d5d045fb30e5c51d4e60d1
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-07 00:55:23 +0900

    変更 - 新たに都道府県がチェックされた時に、データを取得できているか

--- src/feature/PopulationChart/__tests__/hook/usePopulation.test.tsx
+++ src/feature/PopulationChart/__tests__/hook/usePopulation.test.tsx
@@ -1,11 +1,12 @@
 import { describe, test, expect } from 'vitest';
 import { RecoilRoot } from 'recoil';
-import { renderHook, waitFor } from '@testing-library/react';
+import { act, renderHook, waitFor } from '@testing-library/react';
 
 import { setupMockServer } from '@/src/mock/setup';
 import { populationsHandler } from '@/src/feature/PopulationChart/mock/pupilation';
 
 import { usePopulation } from '@/src/feature/PopulationChart/hook/usePopulation';
+import { useSelectedPrefectures } from '@/src/feature/PopulationChart/hook/useSelectedPrefectures';
 
 describe('usePopulation Hook TEST', () => {
   setupMockServer([populationsHandler()]);
@@ -17,6 +18,33 @@ describe('usePopulation Hook TEST', () => {
 
     await waitFor(() => {
       expect(result.current.length).toEqual(0);
+    });
+  });
+  test('チェックされた都道府県がある時、APIからデータを取得しているか', async () => {
+    const { result } = renderHook(
+      () => {
+        const population = usePopulation();
+        const [, setPref] = useSelectedPrefectures();
+
+        return { population, setPref };
+      },
+      {
+        wrapper: RecoilRoot,
+      }
+    );
+
+    await waitFor(() => {
+      expect(result.current.population.length).toEqual(0);
+    });
+
+    await act(async () => {
+      await waitFor(() => {
+        result.current.setPref({ checked: true, value: 1, name: 'Mock' });
+      });
+    });
+
+    await waitFor(() => {
+      expect(result.current.population.length).toEqual(1);
     });
   });
 });
