b52265821b831f7945f1fcada13a5d366ba83fef
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-06 15:43:42 +0900

    変更 - 元の構成を破棄し、専用のマークアップを組み込み始める

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import style from './App.module.scss';
 
-function App() {
-  const [count, setCount] = useState(0);
+import { PopulationChart } from '@/src/feature/PopulationChart';
 
+function App() {
   return (
     <section className={`${style.root}`}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className={`${style.logo}`} alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img
-            src={reactLogo}
-            className={`${style.logo} ${style.react}`}
-            alt="React logo"
-          />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className={`${style.cart}`}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className={`${style.readTheDocs}`}>
-        Click on the Vite and React logos to learn more
-      </p>
+      <PopulationChart />
     </section>
   );
 }
