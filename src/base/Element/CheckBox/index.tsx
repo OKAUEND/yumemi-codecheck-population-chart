3347fceee69413cd25c2337f09277bab5ed2f20b
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-06 18:29:35 +0900

    変更 - CheckBoxのイベントで渡す値を、ReactEventから、任意の値へ絞り込みを行う すべて渡す必要がないので、必要なものだけ渡す方へ変更

--- src/base/Element/CheckBox/index.tsx
+++ src/base/Element/CheckBox/index.tsx
@@ -1,21 +1,29 @@
 import style from './checkbox.module.scss';
+import { SelectedCheckbox } from '@/src/types/Element';
 
 interface Props {
   label: string;
-  checked: boolean;
-  value: string | number;
-  changeHandler: () => void;
+  value: number;
+  changeHandler: ({ checked, value, name }: SelectedCheckbox) => void;
 }
 
-export const CheckBox = ({ label, checked, value, changeHandler }: Props) => {
+export const CheckBox = ({ label, value, changeHandler }: Props) => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const selected: SelectedCheckbox = {
+      checked: event.target.checked,
+      value: value,
+      name: label,
+    };
+    changeHandler(selected);
+  };
+
   return (
     <label className={style.label}>
       <input
         className={style.checkbox}
         type="checkbox"
         value={value}
-        checked={checked}
-        onChange={changeHandler}
+        onChange={onChange}
       />
       <span className={style.text}> {label}</span>
     </label>
