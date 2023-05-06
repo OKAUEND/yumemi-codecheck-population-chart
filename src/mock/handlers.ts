9e92523f684a5caf61df0fdb2a8eb3f77d1f3aff
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-06 15:36:52 +0900

    変更 - エラーを明示的に出すため、オプションを除外する

--- src/mock/handlers.ts
+++ src/mock/handlers.ts
@@ -1,4 +1,3 @@
-import { rest } from 'msw';
 import { prefecturesHandler } from '@/src/feature/PopulationChart/mock/predectures';
 
 export const handlers = [prefecturesHandler()];
--- src/mock/setup.ts
+++ src/mock/setup.ts
@@ -1,11 +1,12 @@
 import { afterAll, afterEach, beforeAll } from 'vitest';
 import { RequestHandler } from 'msw';
 import { setupServer } from 'msw/node';
+import 'whatwg-fetch';
 
 export const setupMockServer = (handler: RequestHandler[]) => {
   const server = setupServer(...handler);
   beforeAll(() => {
-    server.listen({ onUnhandledRequest: 'error' });
+    server.listen();
   });
   afterAll(() => server.close());
   afterEach(() => server.resetHandlers());
