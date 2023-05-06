8a0f27d7152ad301c619e8c29be2e35164fbe181
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-06 18:43:19 +0900

    変更 - CheckBoxで選択した状態を管理できるようにMapオブジェクトで管理をする 追加と削除及び重複処理などが楽なMapオブジェクトで管理。 atomFamilyだと、indexを保有し、ループで状態管理を回さなくてはいけないのでこっちのほうがシンプル

--- src/feature/PopulationChart/hook/useSelectedPrefectures.ts
+++ src/feature/PopulationChart/hook/useSelectedPrefectures.ts
@@ -1,3 +1,42 @@
-import { atom, selector } from 'recoil';
+import { atom, useRecoilCallback, useRecoilValue } from 'recoil';
+import { Prefectures } from '@/src/types/resas';
+import { SelectedCheckbox } from '@/src/types/Element';
 
-export const useSelectedPrefectures = () => {};
+const selectedPrefectures = atom<Map<number, Prefectures>>({
+  key: 'state/selected-prefectures',
+  default: new Map(),
+});
+
+export const useSelectedPrefectures = () => {
+  const selectedPref = useRecoilValue(selectedPrefectures);
+
+  const selectPrefectures = useRecoilCallback(
+    ({ set }) =>
+      (target: SelectedCheckbox) => {
+        if (target.checked) {
+          set(selectedPrefectures, (prev) => {
+            //元のに変更を加えたくないので、クローンを作る
+            const clone = new Map(prev);
+            //お試しなので、キーと内容を同じにする
+            clone.set(target.value, {
+              prefCode: target.value,
+              prefName: target.name,
+            });
+            //setter関数へ返す
+            return clone;
+          });
+        } else {
+          set(selectedPrefectures, (prev) => {
+            //元のに変更を加えたくないので、クローンを作る
+            const clone = new Map(prev);
+            //クローンに対して、チェックを外した内容を削除する
+            clone.delete(target.value);
+            //setter関数へ返す
+            return clone;
+          });
+        }
+      }
+  );
+
+  return [selectedPref, selectPrefectures] as const;
+};
