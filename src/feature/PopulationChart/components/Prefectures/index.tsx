fdda98b53ba659082a1851064d3786cc0989a5ee
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-07 21:44:25 +0900

    変更 - スマホ画面では2列、その他の画面では4列になるように調整する

--- src/feature/PopulationChart/components/Prefectures/index.tsx
+++ src/feature/PopulationChart/components/Prefectures/index.tsx
@@ -1,12 +1,13 @@
 import { CheckBoxList } from '@/src/base/Parts/CheckBoxList';
 import { usePrefectures } from '@/src/feature/PopulationChart/hook/usePrefectures';
 import { useSelectedPrefectures } from '@/src/feature/PopulationChart/hook/useSelectedPrefectures';
+import stlye from '@/src/feature/PopulationChart/styles/components/prefectures.module.scss';
 
 export const PrefectureList = () => {
   const prefectures = usePrefectures();
   const [, selectPrefectures] = useSelectedPrefectures();
   return (
-    <article>
+    <article className={stlye.prefectures}>
       <h2>都道府県</h2>
       <CheckBoxList values={prefectures} changeHandler={selectPrefectures} />
     </article>
