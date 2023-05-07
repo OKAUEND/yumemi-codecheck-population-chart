5180ff72137e0a68957e5dfc4635c4bbee63ae18
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-07 21:43:15 +0900

    環境 - リセットCSSを導入する

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RecoilRoot } from 'recoil';
 import App from './App.tsx';
-import './index.css';
+import 'sanitize.css';
+import './index.module.scss';
 
 if (import.meta.env.MODE === 'development') {
   const { worker } = await import('./mock/browser.ts');
