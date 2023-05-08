26ec58bb0c1f75806a7079aeab502f83c9bd5511
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-08 17:56:09 +0900

    変更 - GithubPagesで公開するために、リポジトリのBaseURLを設定

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/yumemi-codecheck-population-chart/',
   resolve: {
     alias: {
       '@/': `${__dirname}/`,
