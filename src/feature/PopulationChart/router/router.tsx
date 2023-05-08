772ac9cbd0b2841af3f34461da241b2de1a79525
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-08 11:43:12 +0900

    変更 - ErrorBoundaryを設置する

--- src/feature/PopulationChart/router/router.tsx
+++ src/feature/PopulationChart/router/router.tsx
@@ -1,18 +1,22 @@
+import { Suspense } from 'react';
 import style from '@/src/feature/PopulationChart/styles/router.module.scss';
 
 import { PrefectureList } from '@/src/feature/PopulationChart/components/Prefectures';
 import { PopulationChart } from '@/src/feature/PopulationChart/components/PopulationChart';
-import { Suspense } from 'react';
+
+import { ErrorBoundaryExtended } from '@/src/base/Parts/Error';
 
 export const PopulationInfo = () => {
   return (
     <section className={style.population}>
-      <Suspense fallback={<div>Loading...</div>}>
-        <PrefectureList />
-      </Suspense>
-      <Suspense fallback={<div>Loading...</div>}>
-        <PopulationChart />
-      </Suspense>
+      <ErrorBoundaryExtended>
+        <Suspense fallback={<div>Loading...</div>}>
+          <PrefectureList />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <PopulationChart />
+        </Suspense>
+      </ErrorBoundaryExtended>
     </section>
   );
 };
