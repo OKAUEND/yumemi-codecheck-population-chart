44034d17768838142d82e25266ecb68e5894059b
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-11 10:02:10 +0900

    変更 - Github上だとファイルがリネームされていないので、ファイル名を元に戻す

--- src/feature/PopulationChart/mock/prefectures.ts
+++ src/feature/PopulationChart/mock/prefectures.ts
@@ -1,5 +1,5 @@
 import { rest } from 'msw';
-import { ResasResponse, Prefectures } from '@/src/types/Resas.ts';
+import { ResasResponse, Prefectures } from '@/src/types/resas';
 
 const path = () => 'https://opendata.resas-portal.go.jp/api/v1/prefectures';
 
