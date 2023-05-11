6d38b63a4c5256cbda9e0897d60898d05c870bb1
Renoa <38197580+OKAUEND@users.noreply.github.com>
2023-05-11 10:35:52 +0900

    変更 - Buildエラーになっており、SCSSファイルが見つからないBuild設定を修正

--- src/base/Parts/CheckBoxList/index.tsx
+++ src/base/Parts/CheckBoxList/index.tsx
@@ -1,7 +1,8 @@
 import { CheckBox } from '@/src/base/Element/CheckBox';
 import { Prefectures } from '@/src/types/resas';
 import { SelectedCheckbox } from '@/src/types/Element';
-import styles from './checkboxList.module.scss';
+
+import styles from '@/src/base/Parts/CheckBoxList/checkboxList.module.scss';
 
 interface Props {
   values: Prefectures[];
